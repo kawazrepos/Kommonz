34e97a4d676d70321297eaaa5a2786ad493a5093
Λlisue <lambdalisue@hashnote.net>
2011-09-17 21:12:23 +0900

    Add project required skeleton files

--- src/Kommonz/settings.py
+++ src/Kommonz/settings.py
@@ -1,22 +1,41 @@
+# -*- coding: utf-8 -*-
 # Django settings for Kommonz project.
+
+# Load pre local_settings
+try:
+    from local_site import *
+except ImportError:
+    LOCAL_SITE_LOADED = False
+ROOT = os.path.join(os.path.dirname(__file__), '../../')
+#--- Add PYTHON_PATH ---------------------------------
+PYTHON_PATHS = (
+    os.path.join(ROOT, 'src/libs'),
+)
+for path in PYTHON_PATHS:
+    if path not in sys.path: sys.path.append(path)
+#-----------------------------------------------------
+
+    
+# Version of Kommonz
+VERSION = '0.0'
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@domain.com'),
+    # ('Your Name', 'your_email@example.com'),
 )
 
 MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/Users/giginet/Documents/aptana3/Kommonz/src/sqlite.db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE':   'django.db.backends.sqlite3',   # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME':     os.path.join(ROOT, 'kommonz.db'), # Or path to database file if using sqlite3.
+        'USER':     '',                             # Not used with sqlite3.
+        'PASSWORD': '',                             # Not used with sqlite3.
+        'HOST':     '',                             # Set to empty string for localhost. Not used with sqlite3.
+        'PORT':     '',                             # Set to empty string for default. Not used with sqlite3.
     }
 }
 
@@ -27,11 +46,11 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Asia/Tokyo'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
 SITE_ID = 1
 
@@ -43,44 +62,84 @@ USE_I18N = True
 # calendars according to the current locale
 USE_L10N = True
 
-# Absolute path to the directory that holds media.
-# Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ''
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/home/media/media.lawrence.com/media/"
+MEDIA_ROOT = os.path.join(ROOT, 'statics/media'),
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash if there is a path component (optional in other cases).
-# Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = ''
-
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+MEDIA_URL = 'media'
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = os.path.join(ROOT, 'static/collected')
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = 'w^13p=tqhpp%23jr435a4tt1g&u7$b#zw&@6oeb)(@t6xo33fq'
+SECRET_KEY = "Make this unique, and don't share it with anybody."
+
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+STATIC_URL = '/static/'
+
+# URL prefix for admin static files -- CSS, JavaScript and images.
+# Make sure to use a trailing slash.
+# Examples: "http://foo.com/static/admin/", "/static/admin/".
+ADMIN_MEDIA_PREFIX = '/static/admin/'
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    os.path.join(ROOT, 'static'),
+)
+
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
+    'mfw.template.loaders.flavour.Loader',
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
+    'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    'mfw.middleware.session.SessionMiddleware',
+    'mfw.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'mfw.middleware.device.DeviceDetectionMiddleware',
+    'mfw.middleware.emoji.DeviceEmojiTranslationMiddleware',
+    'mfw.middleware.flavour.DeviceFlavourDetectionMiddleware',
+    'pagination.middleware.PaginationMiddleware',
+    'qwert.middleware.threadlocals.ThreadLocalsMiddleware',
+    'qwert.middleware.http.Http403Middleware',
+    'qwert.middleware.exception.UserBasedExceptionMiddleware',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.core.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.request",
+    "mfw.core.context_processors.device",
+    "mfw.core.context_processors.flavour",
 )
 
 ROOT_URLCONF = 'Kommonz.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(ROOT, 'templates'),
+    os.path.join(ROOT, 'templates/default'),
 )
 
 INSTALLED_APPS = (
@@ -89,8 +148,53 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.staticfiles',
+    'django.contrib.admin',
+    'django.contrib.admindocs',
+    # Third-party libraries
+    'compress',                     # JavaScript/CSS compress library
+    'reversetag',                   # Useful templatetag library
+    'pagination',                   # Useful paginatin library
+    # Github libraries
+    'qwert',                        # Useful snippet collection library
+    'mfw',                          # Django mobile framework library
+    'object_permission',            # Object permission library
 )
+
+AUTHENTICATION_BACKENDS = (
+    'qwert.backends.auth.EmailAuthBackend',
+    'django.contrib.auth.backends.ModelBackend',
+    'object_permission.backends.ObjectPermBackend',
+)
+LOGIN_REDIRECT_URL  = "/"
+LOGIN_URL = "/registration/login/"
+LOGOUT_URL = "/registration/logout/"
+
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
+
+# Load local_settings
+try:
+    from local_settings import *
+except ImportError:
+    LOCAL_SETTINGS_LOADED = False
