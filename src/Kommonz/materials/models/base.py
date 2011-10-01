1ed7d09b4c1b063098aecfa048256c4296e4cf5b
giginet <giginet.net@gmail.com>
2011-10-02 02:41:39 +0900

    implemented KERO, License, Creative Commons models.

--- src/Kommonz/materials/models/base.py
+++ src/Kommonz/materials/models/base.py
@@ -12,31 +12,78 @@ class MaterialManager(models.Manager):
     pass
 
 class Material(models.Manager):
-    u"""abstract model of whole materials."""
+    u"""
+        abstract model of whole materials.
+    """
     
     def get_file_path(self, filename):
         return filename
     
     # required
-    label       = models.CharField(_('label'), max_length=128)
-    description = models.TextField(_('description'))
-    file        = models.FilePathField(_('file'), path=get_file_path)
+    label       = models.CharField(_('Label'), max_length=128)
+    description = models.TextField(_('Description'))
+    file        = models.FilePathField(_('File'), path=get_file_path)
     
     # auto add
     created_at  = models.DateTimeField(_('Created At'), auto_now_add=True)
     updated_at  = models.DateTimeField(_('Updated At'), auto_now=True)
-    author      = models.ForeignKey(User, verbose_name=_('author'), editable=False)
+    author      = models.ForeignKey(User, verbose_name=_('author'), editable=False, related_name="materials")
     pv          = models.PositiveIntegerField(_('Page View'), default=0, editable=False)
     download    = models.PositiveIntegerField(_('Download Count'), default=0, editable=False)
     ip          = models.IPAddressField(_('IP Address'), editable=False)
+    license     = models.ForeignKey('License', verbose_name=_('License'))
     
     object      = MaterialManager()
     
     class Meta:
-        ordering            = '-created_at'
+        ordering            = ('-created_at',)
         verbose_name        = _('Material')
         verbose_name_plural = _('Materials')
         
     @models.permalink
     def get_absolute_url(self):
         return ""
+
+class Kero(object):
+    u"""
+        Kero is a rating system for Materials.
+    """
+    
+    def get_file_path(self, filename):
+        return filename
+    
+    label       = models.CharField(_('Label'), max_length=32)
+    description = models.TextField(_('Description'))
+    icon        = models.FilePathField(_('Icon'), path=get_file_path)
+    materials   = models.ManyToManyField(Material, related_name='keros', verbose_name=_('materials'))
+    
+    class Meta:
+        ordering            = ('pk',)
+        verbose_name        = _('KERO')
+        verbose_name_plural = verbose_name
+
+class License(models.Model):
+    u"""
+        License
+    """
+    
+    label        = models.CharField(_('Label'), max_length=32)
+    description  = models.TextField(_('Description'))
+
+    class Meta(License.Meta):
+        verbose_name        = _('License')
+        verbose_name_plural = _('Licenses') 
+
+class CreativeCommons(License):
+    u"""
+        CreativeCommons http://en.wikipedia.org/wiki/Creative_Commons
+    """
+
+    noncommerical = models.BooleanField(_('Noncommerical'), default=False)
+    no_derivative = models.BooleanField(_('No Derivative Works'), default=False)
+    share_alike   = models.BooleanField(_('Share Alike'), default=False)
+    lisence       = models.OneToOneField(Material, verbose_name=_('Creative Commons'), related_name='commons')
+    
+    class Meta(License.Meta):
+        verbose_name        = _('CreaticeCommons')
+        verbose_name_plural = verbose_name
