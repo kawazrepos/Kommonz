2bad819cd50b084d051229e6d9ae6b893b0e0088
tohhy <clear9159@gmail.com>
2011-10-31 21:21:23 +0900

    notification dropdown timeline prototype implemented

--- src/Kommonz/notifications/templatetags/notifications.py
+++ src/Kommonz/notifications/templatetags/notifications.py
@@ -8,3 +8,16 @@ def notification_count(context):
     request = context['request']
     return Notification.objects.filter(user_to=request.user, solved=False).count()
     
+
+@register.inclusion_tag('notifications/components/message_notification_list.html',
+                         takes_context=True)
+def notification_list(context):
+    
+    request = context['request']
+    
+    object_list = Notification.objects.filter(user_to=request.user, solved=False)
+    
+    context['object_list'] = object_list
+                      
+    return context
+ 
