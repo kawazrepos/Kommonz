b2ce38cde2635bc5c4695ab7c435d0de4b47652d
giginet <giginet.net@gmail.com>
2011-11-09 15:29:09 +0900

    work in zipview

--- src/Kommonz/materials/views/zip.py
+++ src/Kommonz/materials/views/zip.py
@@ -3,9 +3,18 @@
 # zip.py
 # created by giginet on 2011/11/09
 #
-from django.views.generic import DetailView
+import tmpfile
+import zipfile
+from django.views.generic.base import TemplateResponseMixin
+from django.views.generic.list import BaseListView
 
-class MaterialZipView(DetailView):
+class MultipleMaterialZipResponseMixin(TemplateResponseMixin):
+    """
+        A mixin that can be used to zip a materials
+    """
+    pass
+
+class MaterialZipView(MultipleMaterialZipResponseMixin, BaseListView):
     """
         generate zip archive from queryset.
     """
