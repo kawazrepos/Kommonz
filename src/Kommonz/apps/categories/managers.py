beb7bcdc92f568a734da97782e5ae13b6158a1f4
tohhy <clear9159@gmail.com>
2011-11-19 14:55:30 +0900

    get_filetype_category implemented

--- src/Kommonz/apps/categories/managers.py
+++ src/Kommonz/apps/categories/managers.py
@@ -3,6 +3,34 @@ from django.db import models
 from django.db.models import Q
 
 class CategoryManager(models.Manager):
+    def get_children_tree(self):
+        from models import Category
+        def _get_dict(category):
+            categories = set([category,])
+            for child in category.children.iterator():
+                categories.update(_get_set(child))
+            caregory_dict = {category : categories}
+            categories = set([category,])
+            for child in category.children.iterator():
+                categories.update(_get_set(child)) 
+        
+    
+    def get_filetype_category(self, filename):
+        from django.contrib.contenttypes.models import ContentType
+        from django.core.exceptions import ObjectDoesNotExist
+        from apps.materials.utils import filetypes
+        from models import Category
+        model = filetypes.get_file_model(filename)
+        model_ct = ContentType.objects.get_for_model(model)
+        try:
+            category = Category.objects.get(Q(label=model_ct.name),
+                                            Q(parent=None))
+            return category
+        except ObjectDoesNotExist:
+            category = Category.objects.create(label=model_ct.name)
+            return category
+
+    
     def get_children(self, category):
         from models import Category
         def _get_set(category):
@@ -13,6 +41,20 @@ class CategoryManager(models.Manager):
         queries = [Q(parent=category) for category in _get_set(category)]
         query = reduce(lambda a, b : a | b, queries)
         return Category.objects.filter(query)
+    
+    
+    def get_parents(self, category):
+        from models import Category
+        def _get_set(category):
+            categories = set([category,])
+            if category.parent:
+                categories.update(_get_set(category.parent))
+            return categories
+        categories = _get_set(category).remove(category)
+        queries = [Q(pk=category.pk) for category in _get_set(categories)]
+        query = reduce(lambda a, b : a | b, queries)
+        return Category.objects.filter(query)
+    
     
 #    def get_parents(self, instance):
 #        results = instance.parent
--- src/Kommonz/apps/categories/tests.py
+++ src/Kommonz/apps/categories/tests.py
@@ -8,17 +8,24 @@ from django.test.client import Client
 from nose.tools import *
 from apps.categories.models import Category
 
+
 class TestCategoryManager(object):
     def setup(self):
         self.category0 = Category.objects.create(label="Image")
         self.category1 = Category.objects.create(label="Shooting", parent=self.category0)
         self.category2 = Category.objects.create(label="RPG", parent=self.category0)
         self.category3 = Category.objects.create(label="Enemy", parent=self.category1)
+        self.category4 = Category.objects.create(label="Item", parent=self.category1)
         self.category5 = Category.objects.create(label="Bullet", parent=self.category1)
         self.category6 = Category.objects.create(label="Map", parent=self.category2)
         self.category7 = Category.objects.create(label="Town", parent=self.category6)
         self.category8 = Category.objects.create(label="Dungeon", parent=self.category6)
         self.category9 = Category.objects.create(label="Forest", parent=self.category8)
+        
+    def teardown(self):
+        for i in xrange(0, 9):
+            category = getattr(self, "category%d" % i)
+            category.delete()
 
     def test_get_children(self):
         """
@@ -26,8 +33,35 @@ class TestCategoryManager(object):
         """
         shooting = Category.objects.get_children(self.category1)
         ok_(isinstance(shooting, QuerySet))
-        eq_(shooting.count(), 2)
+        eq_(shooting.count(), 3)
         rpg = Category.objects.get_children(self.category2)
         eq_(rpg.count(), 4)
         forest = Category.objects.get_children(self.category9)
         eq_(forest.count(), 0)
+
+    def test_get_parents(self):
+        """
+        Tests manager can get a category's parents.
+        """
+        forest = Category.objects.get_parents(self.category9)
+        ok_(isinstance(forest, QuerySet))
+        eq_(forest.count(), 4)
+        map = Category.objects.get_children(self.category6)
+        eq_(map.count(), 2)
+        image = Category.objects.get_children(self.category0)
+        eq_(image.count(), 0)
+        
+        
+    def test_get_filetype_category(self):
+        """
+        Tests manager can get a filename suitable category.
+        """
+        print Category.objects.all()
+        image_category = Category.objects.get_filetype_category("kawaztan.png")
+        ok_(isinstance(image_category, Category))
+        eq_(image_category.label, 'Image')
+        audio_category = Category.objects.get_filetype_category("kawaztan_theme.mp3")
+        eq_(audio_category.label, 'Audio')
+        code_category = Category.objects.get_filetype_category("create_kawaz.py")
+        eq_(code_category.label, 'Code')
+        
