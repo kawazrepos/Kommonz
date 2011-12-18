4fe3fce0c584edccdb2d75e71f4842dc6a44644a
tohhy <clear9159@gmail.com>
2011-12-18 14:00:21 +0900

    added some fixtures

--- src/Kommonz/apps/categories/admin.py
+++ src/Kommonz/apps/categories/admin.py
@@ -3,6 +3,6 @@ from apps.categories.models import Category
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display    = ('__unicode__', 'parent',)
-    ordering        = ['parent']
+    ordering        = ['id']
 
 admin.site.register(Category, CategoryAdmin)
